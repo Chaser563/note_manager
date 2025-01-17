4949ad931ffbfcac1abc62e94db6c7470ca5c4a7
Chaser563 <vista2000@list.ru>
2025-01-17 19:10:52 +0400

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = 'Николай'
+title = 'Не пропустить вечерние занятия'
+content = 'После ужина садиться за учебу'
+status = 'Активна'
+created_date = '17.01.2025'
+issue_date = '19.01.2015'
+
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(issue_date)
